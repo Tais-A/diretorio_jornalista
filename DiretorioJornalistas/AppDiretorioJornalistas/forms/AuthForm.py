8d3522b1bc36334784d5e90e19dd55638bf92c5f
Tais-A <dtais@outlook.com.br>
2022-06-26 20:18:23 -0300

    placeholder registro

--- DiretorioJornalistas/AppDiretorioJornalistas/forms/AuthForm.py
+++ DiretorioJornalistas/AppDiretorioJornalistas/forms/AuthForm.py
@@ -3,10 +3,10 @@ from django import forms
 
 class LoginForm(forms.Form):
     #email = forms.CharField(required=True, widget=forms.TextInput(attrs={'id':'caixa-login','class': 'login', 'placeholder':'login'}))
-    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'id':'caixa-login','class': 'form-control', 'placeholder':'Login'}))
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'id':'caixa-login','class': 'form-control', 'placeholder':'Usuário'}))
     password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'Senha'}), required=True)
         
 class RegisterForm(forms.Form):
-    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'type':'email'}))
-    password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'class': 'form-control'}), required=True)
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Usuário'}))
+    email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder':'Email'}))
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'Senha'}), required=True)
